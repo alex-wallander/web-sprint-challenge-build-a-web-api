3d5189e2bdda8469b4988cc60c754236e335be6d
alex-wallander <alexwallander@gmail.com>
2021-03-12 10:51:11 -0600

    wip

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,18 @@
 // Write your "actions" router here!
+const express = require('express');
+
+const router = express.Router();
+
+const Action = require('./actions-model');
+
+router.get('/', (req, res) => {
+    Action.get(req.body)
+    .then(actions => {
+        res.status(200).json(actions)
+    })
+    .catch((err) => {
+        res.status(500).json({ message: err.message })
+    })
+})
+
+module.exports = router;
